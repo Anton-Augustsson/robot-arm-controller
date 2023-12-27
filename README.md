dd61e0ad7f99e3e7d20df6d557d5496625dc65ae
Anton Augustsson <anton.augustsson99@gmail.com>
2023-12-27 18:24:04 +0100

    test with docker container

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# robot-arm-controller
+# Robot Arm Controller
 3D printed robot arm, controlled with a Raspberry Pi Pico.
+
+The docker container that the code is compiled in was created by [lukestep](https://hub.docker.com/r/lukstep/raspberry-pi-pico-sdk), for more instructions view his repo.
+
+Run the following command in the repos root directory
+```
+docker run -d -it --name pico-sdk --mount type=bind,source=${PWD},target=/home/dev lukstep/raspberry-pi-pico-sdk:latest
+```
---
+++ main.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "hardware/gpio.h"
+#include "pico/binary_info.h"
+
+const int LED_PIN = 25;
+
+
+int main ()
+{
+    bi_decl(bi_program_description("Sample binary"));
+    bi_decl(bi_1pin_with_name(LED_PIN, "on-board PIN"));
+
+    stdio_init_all();
+
+    gpio_init(LED_PIN);
+    gpio_set_dir(LED_PIN, GPIO_OUT);
+
+    while(1)
+    {
+        gpio_put(LED_PIN, 0);
+        sleep_ms(250);
+        gpio_put(LED_PIN, 1);
+        puts("Hello Word\n");
+        sleep_ms(1000);
+    }
+}
+//#include "pico/stdlib.h"
+//
+//int main() {
+//#ifndef PICO_DEFAULT_LED_PIN
+//#warning blink example requires a board with a regular LED
+//#else
+//    const uint LED_PIN = PICO_DEFAULT_LED_PIN;
+//    gpio_init(LED_PIN);
+//    gpio_set_dir(LED_PIN, GPIO_OUT);
+//    while (true) {
+//        gpio_put(LED_PIN, 1);
+//        sleep_ms(250);
+//        gpio_put(LED_PIN, 0);
+//        sleep_ms(250);
+// }
+// #end
