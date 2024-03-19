a865ab18d060793449e8ec6184ac711854d0d212
Anton Augustsson <anton.augustsson99@gmail.com>
2024-03-19 21:32:52 +0100

    more resources and info in README, and added SPI support for pico

--- README.md
+++ README.md
@@ -11,8 +11,22 @@ docker run -d -it --name pico-sdk --mount type=bind,source=${PWD},target=/home/d
 ## Framework 
 Framework is a separate program to experiment the general program structure of a microcontroller.
 
+![Alt text](documentation/images/spi_diagram2.png "Multiple SPI")
+
 ## Requirement
 For src you need to install (nlohmann/json)[https://github.com/nlohmann/json/tree/develop?tab=readme-ov-file#integration], which you can do with [vcpkg](https://github.com/Microsoft/vcpkg/?tab=readme-ov-file#quick-start-unix).
 
 ## Resources
-[Step 1: A Basic Starting Point](https://cmake.org/cmake/help/latest/guide/tutorial/A%20Basic%20Starting%20Point.html)
+- [Step 1: A Basic Starting Point](https://cmake.org/cmake/help/latest/guide/tutorial/A%20Basic%20Starting%20Point.html)
+- [Google C++ Style Guide](https://google.github.io/styleguide/cppguide.html)
+
+### PICO SDK
+- [Raspberry Pi Pico C/C++ SDK](https://datasheets.raspberrypi.com/pico/raspberry-pi-pico-c-sdk.pdf)
+- [Getting started with Raspberry Pi Pico](https://datasheets.raspberrypi.com/pico/getting-started-with-pico.pdf)
+- [Making Two Raspberry Pi Pico Boards Communicate through SPI using C/C++ SDK](https://www.circuitstate.com/tutorials/making-two-raspberry-pi-pico-boards-communicate-through-spi-using-c-cpp-sdk/)
+
+### Linux driver for SPI peripheral
+- [SPI – Serial Peripheral Interface Protocol Basics](https://embetronicx.com/tutorials/tech_devices/spi-serial-peripheral-interface-protocol-basics/)
+- [Linux Device Driver Tutorial – Part 1 | Introduction](https://embetronicx.com/tutorials/linux/device-drivers/linux-device-driver-part-1-introduction/)
+- [Serial Peripheral Interface (SPI)](https://www.kernel.org/doc/html/v4.13/driver-api/spi.html)
+- [Device Tree for Dummies](https://elinux.org/images/f/f9/Petazzoni-device-tree-dummies_0.pdf)
--- main.cpp
+++ main.cpp
@@ -1,46 +1,58 @@
 #include <stdio.h>
-#include "pico/stdlib.h"
+#include "hardware/spi.h"
 #include "hardware/gpio.h"
+#include "pico/stdlib.h"
 #include "pico/binary_info.h"
 #include "src/motor/servo_motor.hpp" 
 #include "src/motor/mg996r.hpp" 
 #include "src/utils/types.hpp" 
 
-const int LED_PIN = 25;
-auto test = 2;
+#define LED_PIN 25
+#define BUF_LEN 128
+
 
 int main ()
 {
-    bi_decl(bi_program_description("Sample binary"));
-    bi_decl(bi_1pin_with_name(LED_PIN, "on-board PIN"));
+  bi_decl(bi_program_description("Sample binary"));
+  bi_decl(bi_1pin_with_name(LED_PIN, "on-board PIN"));
 
+  MotorParameters motorParameters0 = {"mg996r", 0, PwmRange {3, 12}, AngleRange {0, 90}};
+  Mg996r motor0 = Mg996r(motorParameters0);
 
-    MotorParameters motorParameters0 = {
-      "mg996r",
-      0,
-      PwmRange {
-        3,
-        12,
-      },
-      AngleRange {
-        0,
-        90,
-      },
-    };
+  stdio_init_all();
 
-    Mg996r motor0 = Mg996r(motorParameters0);
+  gpio_init(LED_PIN);
+  gpio_set_dir(LED_PIN, GPIO_OUT);
 
-    stdio_init_all();
+  sleep_ms (2 * 1000);
+  printf ("SPI Peripheral Example\n");
 
-    gpio_init(LED_PIN);
-    gpio_set_dir(LED_PIN, GPIO_OUT);
+  // Enable SPI 0 at 1 MHz and connect to GPIOs
+  spi_init (spi_default, 1 * 1000000);
+  spi_set_slave (spi_default, true);
 
-    while(1)
-    {
-        gpio_put(LED_PIN, 0);
-        sleep_ms(250);
-        gpio_put(LED_PIN, 1);
-        puts("Hello Word\n");
-        sleep_ms(1000);
+  gpio_set_function (PICO_DEFAULT_SPI_RX_PIN, GPIO_FUNC_SPI);
+  gpio_set_function (PICO_DEFAULT_SPI_SCK_PIN, GPIO_FUNC_SPI);
+  gpio_set_function (PICO_DEFAULT_SPI_TX_PIN, GPIO_FUNC_SPI);
+  gpio_set_function (PICO_DEFAULT_SPI_CSN_PIN, GPIO_FUNC_SPI);
+
+  uint8_t out_buf [BUF_LEN], in_buf [BUF_LEN];
+
+  // Initialize output buffer
+  for (uint8_t i = 0; i < BUF_LEN; ++i) {
+    out_buf [i] = 0;
+    in_buf [i] = 0;
+  }
+
+  while (1) {
+    gpio_put(LED_PIN, 0);
+    if (spi_is_readable (spi_default)) {
+      printf ("Reading data from SPI..\n");
+      // Write the output buffer to MOSI, and at the same time read from MISO.
+      spi_read_blocking (spi_default, 0, in_buf, 1);
+      gpio_put(LED_PIN, 1);
+
+      printf ("Data received: %d\n", in_buf [0]);
     }
+  }
 }
